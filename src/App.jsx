aeeca71ee1c7e54282a14bf43ca800f61bd8ac8d
KothaSriya23 <2300032275@kluniversity.in>
2025-08-24 19:17:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,12 @@ import './App.css'
 import Login from './components/Login';
 import Signup from './components/Signup';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './components/HomePage';
+import HomePage from './components/HomePage';   // 👈 this is your main layout page
 import AboutUs from './components/AboutUs';
 import Computers from './components/Computers';
 import Mobiles from './components/Mobiles';
 import Laptops from './components/Laptops';
 import Pendrives from './components/Pendrives';
-import Home from './components/Home';
 import ProductPage from './components/ProductPage';
 import Cart from './components/Cart';
 import Payment from './components/Payment';
@@ -20,30 +19,29 @@ function App() {
 
   return (
     <>
-      {/* 👇 Important: basename matches your Tomcat context path */}
+      {/* basename must match Tomcat context */}
       <BrowserRouter basename="/ecommerce-frontend">
         <Routes>
-          {/* Main routes */}
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/about-us" element={<AboutUs />} />
-          <Route path="/computers" element={<Computers />} />
-          <Route path="/mobiles" element={<Mobiles />} />
-          <Route path="/laptops" element={<Laptops />} />
-          <Route path="/pendrives" element={<Pendrives />} />
+          {/* Root should render HomePage (with sidebar, header, etc.) */}
+          <Route path="/" element={<HomePage />}>
+            <Route index element={<AboutUs />} />
+            <Route path="about-us" element={<AboutUs />} />
+            <Route path="computers" element={<Computers />} />
+            <Route path="mobiles" element={<Mobiles />} />
+            <Route path="laptops" element={<Laptops />} />
+            <Route path="pendrives" element={<Pendrives />} />
+            <Route path="productpage" element={<ProductPage />} />
+            <Route path="cart" element={<Cart />} />
+            <Route path="payment" element={<Payment />} />
+            <Route path="orders" element={<Orders />} />
+          </Route>
 
           {/* Auth routes */}
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
 
-          {/* E-commerce flow */}
-          <Route path="/productpage" element={<ProductPage />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/payment" element={<Payment />} />
-          <Route path="/orders" element={<Orders />} />
-
-          {/* Catch-all fallback route (optional) */}
-          <Route path="*" element={<Home />} />
+          {/* Fallback to HomePage */}
+          <Route path="*" element={<HomePage />} />
         </Routes>
       </BrowserRouter>
     </>
