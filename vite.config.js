28bdcc5dbe1d546f1ecbc6670f545531aa605957
KothaSriya23 <2300032275@kluniversity.in>
2025-08-24 19:12:47 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+// ⚡ Important: base must match Tomcat app folder name
 export default defineConfig({
   plugins: [react()],
-  base: '/ecommerce-frontend/',
+  base: '/ecommerce-frontend/',   // ensures correct paths in production
 })
